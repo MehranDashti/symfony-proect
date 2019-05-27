bf36f28b79106487da4dc11532423d33d0401f71
MehranDahsti <mehran19.md@gmail.com>
2019-05-27 19:03:15 +0430

    New Entity has been written for profile and make the neccessary migration and repository for it

---
+++ src/Entity/Profile.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProfileRepository")
+ */
+class Profile
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\user", cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user_id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $family;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $id_card;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?user
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(user $user_id): self
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFamily(): ?string
+    {
+        return $this->family;
+    }
+
+    public function setFamily(?string $family): self
+    {
+        $this->family = $family;
+
+        return $this;
+    }
+
+    public function getIdCard(): ?string
+    {
+        return $this->id_card;
+    }
+
+    public function setIdCard(?string $id_card): self
+    {
+        $this->id_card = $id_card;
+
+        return $this;
+    }
+}
